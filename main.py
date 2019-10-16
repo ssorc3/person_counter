0fdd7b49e5fd718de9767043bcbfd24757a57963
Ben Cross <ssorc3@gmail.com>
2019-10-16 14:37:23 +0100

    Add logs

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def sendMessage(peopleDetected):
     print("Detected " + str(peopleDetected) + (" person" if peopleDetected == 1 else " people"))
 
 # Read the graph.
+print("Reading the graph")
 with tf.gfile.FastGFile('frozen_inference_graph.pb', 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
+print("Loaded the graph")
 
 cap = cv.VideoCapture(0)
 
+print("Creating Session")
 with tf.Session() as sess:
     # Restore session
     sess.graph.as_default()
@@ -27,20 +30,23 @@ with tf.Session() as sess:
     while(True):
         if (lastSent + 2 < time.time()):
             peopleDetected = 0
+            print("Getting image")
             ret, img = cap.read()
+            print("Reshaping image")
             rows = img.shape[0]
             cols = img.shape[1]
             inp = cv.resize(img, (300, 300))
             inp = inp[:, :, [2, 1, 0]]  # BGR2RGB
 
             # Run the model
+            print("Running inference")
             out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
                             sess.graph.get_tensor_by_name('detection_scores:0'),
                             sess.graph.get_tensor_by_name('detection_boxes:0'),
                             sess.graph.get_tensor_by_name('detection_classes:0')],
                         feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
 
-            # Visualize detected bounding boxes.
+            print("Inference done")
             num_detections = int(out[0][0])
             for i in range(num_detections):
                 classId = int(out[3][0][i])
